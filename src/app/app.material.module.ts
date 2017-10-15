6f278bda41d4ed7dd9a025997563682a848dbbf9
mmogley <mrrubato@gmail.com>
2017-10-15 12:39:49 -0700

    Updated to angular 4.4.5. Updated to angular-cli 4.4.7 Updated to angular-material 2.0.0-beta.12. Fixed to use mat- prefixes for angular-material components.

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -1,39 +1,39 @@
-import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
 import {
-  MdAutocompleteModule,
-  MdButtonModule,
-  MdCardModule,
-  MdIconModule,
-  MdInputModule, MdListModule,
-  MdMenuModule,
-  MdOptionModule,
-  MdRippleModule,
-  MdSelectModule,
-  MdSidenavModule,
-  MdSlideToggleModule,
-  MdToolbarModule
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatOptionModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatToolbarModule
 } from "@angular/material";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [
-    MdAutocompleteModule,
-    MdButtonModule,
-    MdCardModule,
-    MdIconModule,
-    MdInputModule,
-    MdListModule,
-    MdMenuModule,
-    MdOptionModule,
-    MdRippleModule,
-    MdSelectModule,
-    MdSidenavModule,
-    MdSlideToggleModule,
-    MdToolbarModule
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatOptionModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatToolbarModule
   ]
 })
 export class AppMaterialModule {
